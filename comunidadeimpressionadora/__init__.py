d24eaf07ed1414e5cfb7e24c88d4dd8866201e6c
andreguedes123 <126020805+andreguedes123@users.noreply.github.com>
2023-03-11 15:40:00 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -18,6 +18,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+if not engine.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("base de dados criada")
+else:
+    print("base de dados já existente")
 
 from comunidadeimpressionadora import routes
 
